f5580a9ea5246821f75add1dcf8019f6f43d54ab
Ông Tùng Dương <ongtungduong@gmail.com>
2025-02-07 10:18:09 +0700

    feature: setup postgres exporter as systemd service

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+EXPORTER_PORT=${EXPORTER_PORT:-15432}
+EXPORTER_VERSION=${EXPORTER_VERSION:-0.16.0}
+
+function install_postgres_exporter() {
+    echo "Downloading Postgres Exporter v${EXPORTER_VERSION}..."
+    curl -LO "https://github.com/prometheus-community/postgres_exporter/releases/download/v${EXPORTER_VERSION}/postgres_exporter-${EXPORTER_VERSION}.linux-amd64.tar.gz"
+    tar -xzf postgres_exporter-${EXPORTER_VERSION}.linux-amd64.tar.gz
+    sudo mv postgres_exporter-${EXPORTER_VERSION}.linux-amd64/postgres_exporter /usr/local/bin/
+
+    echo "Cleaning up..."
+    rm -rf postgres_exporter-${EXPORTER_VERSION}.linux-amd64.tar.gz postgres_exporter-${EXPORTER_VERSION}.linux-amd64
+}
+
+function create_postgres_exporter_user() {
+    echo "Creating postgres_exporter user and directory..."
+    sudo useradd --no-create-home --shell /bin/false postgres_exporter
+    sudo mkdir -p /etc/postgres_exporter
+    sudo cp postgres_exporter.env /etc/postgres_exporter/postgres_exporter.env
+    sudo chown -R postgres_exporter:postgres_exporter /etc/postgres_exporter
+}
+
+function create_postgres_exporter_systemd_service() {
+    echo "Creating postgres_exporter systemd service..."
+    sudo tee /etc/systemd/system/postgres_exporter.service > /dev/null << EOF
+    [Unit]
+    Description=Prometheus Postgres Exporter
+    After=network.target
+
+    [Service]
+    User=postgres_exporter
+    Group=postgres_exporter
+    EnvironmentFile=/etc/postgres_exporter/postgres_exporter.env
+    ExecStart=/usr/local/bin/postgres_exporter --collector.stat_statements --web.listen-address=:$EXPORTER_PORT
+
+    [Install]
+    WantedBy=multi-user.target
+EOF
+}
+
+function enable_and_start_postgres_exporter_service() {
+    echo "Enabling and starting postgres_exporter service..."
+    sudo systemctl daemon-reload
+    sudo systemctl enable postgres_exporter.service --now
+}
+
+function print_success_message() {
+    echo "Postgres Exporter installed successfully!!!"
+    echo "You can now access the metrics at http://localhost:$EXPORTER_PORT/metrics"
+}
+
+install_postgres_exporter
+create_postgres_exporter_user
+create_postgres_exporter_systemd_service
+enable_and_start_postgres_exporter_service
+print_success_message
